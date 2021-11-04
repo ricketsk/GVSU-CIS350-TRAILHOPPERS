6dfba76cb6f331fa7998289f2632067b24e2bee1
ricketsk <84805456+ricketsk@users.noreply.github.com>
2021-11-03 22:32:54 -0400

    Create MapIntegration.py

---
+++ src/MapIntegration.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy_garden.mapview import MapView, MapSource
+
+class MapMainApp(App):
+    
+    def build(self):
+        box_layout = BoxLayout()
+        map_view = MapView(lat=50.3, lon=3.05, zoom=13)
+        map_view.map_source = "osm"
+        box_layout.add_widget(map_view)
+        return box_layout
+
+if __name__ == '__main__':
+    MapMainApp().run()
+
+
+
+
