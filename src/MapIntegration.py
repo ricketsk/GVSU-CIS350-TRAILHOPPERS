0e6fb6c77f71a16cb34a7e7e2e41d5eaa3d96cfc
ricketsk <84805456+ricketsk@users.noreply.github.com>
2021-11-09 23:12:43 -0500

    updated geolocation/broke map app
    updated the code to take in user input of an address and provide the lat and long of the entered address
    
    dont know what happened to the app class - broke it somehow

--- src/MapIntegration.py
+++ src/MapIntegration.py
@@ -1,45 +1,18 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy_garden.mapview import MapView, MapSource
-
-googleApiKey = "AIzaSyBdwkw6tlqH340Br0Hz1h1AieGkQg98f3I"
 
 class MapMainApp(App):
 
+    googleApiKey = "AIzaSyBdwkw6tlqH340Br0Hz1h1AieGkQg98f3I"
+
     def build(self):
+
+        boxLayout = BoxLayout()
+        mapView = MapView(lat=42.9903205, lon=-85.95296849268018, zoom=13)
+        mapView.map_source = "osm"
+        boxLayout.add_widget(mapView)
+        return boxLayout
         
-        box_layout = BoxLayout()
-        map_view = MapView(lat=42.9641, lon=-85.8890, zoom=13)
-        map_view.map_source = "osm"
-        box_layout.add_widget(map_view)
-        return box_layout, Label(text="TrailHoppers")
-
-
-    """def extract_lat_long_via_address(address_or_zipcode):
-        lat, lng = None, None
-        api_key = googleApiKey
-        base_url = "https://maps.googleapis.com/maps/api/geocode/json"
-        endpoint = f"{base_url}?address={address_or_zipcode}&key={api_key}"
-        # see how our endpoint includes our API key? Yes this is yet another reason to restrict the key
-        r = requests.get(endpoint)
-        if r.status_code not in range(200, 299):
-            return None, None
-        try:
-            '''
-            This try block incase any of our inputs are invalid. This is done instead
-            of actually writing out handlers for all kinds of responses.
-            '''
-            results = r.json()['results'][0]
-            lat = results['geometry']['location']['lat']
-            lng = results['geometry']['location']['lng']
-        except:
-            pass
-        return lat, lng """
-
 if __name__ == '__main__':
     MapMainApp().run()
-
-
-
-
---
+++ src/geolocation.py
@@ -0,0 +1,28 @@
+from geopy.location import Location
+from geopy.geocoders import Nominatim
+import time
+from pprint import pprint    
+    
+googleApiKey = "AIzaSyBdwkw6tlqH340Br0Hz1h1AieGkQg98f3I"
+
+userInput = input("Enter your location: ")
+
+app = Nominatim(user_agent="tutorial")
+location = app.geocode(userInput).raw
+pprint(location)
+
+address = userInput
+
+def getLocationByAddress(address):
+    app = Nominatim(user_agent="tutorial2")
+    time.sleep(1)
+    try:
+        return app.geocode(address).raw
+    except:
+        return #getLocationByAddress(address)
+
+location = getLocationByAddress(address)
+latitude = location["lat"]
+longitude = location["lon"]
+print(f"{latitude}, {longitude}")
+pprint(location)
