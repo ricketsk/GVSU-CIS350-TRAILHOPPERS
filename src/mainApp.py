98d25263687888b5ab8e352a4bc9dbdd687d2e21
Mazureli <mazureli@mail.gvsu.edu>
2021-12-03 08:54:56 -0500

    Added user_Name to submit_user

--- src/mainApp.py
+++ src/mainApp.py
@@ -6,7 +6,7 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivymd.theming import ThemeManager
-import Database
+import Database_Trailhoppers as data
 
 class WelcomeWindow(Screen):
     pass 
@@ -24,18 +24,24 @@ class LoginWindow(Screen):
 class StartWindow(Screen):
     firstName = ObjectProperty(None)
     lastName = ObjectProperty(None)
+    user_Name = ObjectProperty(None)
     email = ObjectProperty(None)
     passWord = ObjectProperty(None)
 
     def button(self):
         print("First Name:", self.firstName.text, 
         "Last Name:", self.lastName.text, 
+        "Username:", self.user_Name.text,
         "Email:", self.email.text, 
         "Password:", self.passWord.text)
         self.firstName.text = ""
         self.lastName.text = ""
+        self.user_Name.text = ""
         self.email.text = ""
         self.passWord.text = ""
+    
+    #data.DatabaseMainApp().submit_user(firstName, lastName, user_Name, email, passWord)
+    data.DatabaseMainApp().submit_user("John", "Ham", "JohnHam", "johnham@gamil.com", "password2")
     pass
 
 class MainWindow(Screen):
@@ -56,7 +62,7 @@ class MyApp(MDApp):
         self.theme_cls.accent_palette = "Blue"
         self.theme_cls.theme_style = "Dark"
         Builder.load_file("My.kv")
-        
+        data.DatabaseMainApp().build_db()
         
         
         
